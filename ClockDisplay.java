cf2e488f67eb1d009d6dcc4dce36c739fbaa20a8
KevinFN04 <kevin.felix.04@gmail.com>
2015-11-19 12:36:30 +0100

    Cambiado reloj a formato 12H

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -13,7 +13,10 @@ public class ClockDisplay
     private NumberDisplay minutos;
     //Variable String de 5 digitos con ls hora
     private String horaSt;
-    
+    //variable para definir si es am o pm
+    private String meridian;
+    // horas sobre 24H
+    private int veintH;
 
     /**
      * Constructor para poner la hora a cero al iniciar.
@@ -24,29 +27,42 @@ public class ClockDisplay
         horas.setValue(0);
         minutos = new NumberDisplay(60);
         minutos.setValue(0);
-        horaSt = "00:00";
+        meridian = "am";
+        horaSt = "12:00 am";
     }
     
     /**
      * Constructor para fijar una hora al iniciar.
      */
-    public ClockDisplay(int horaAc, int minutosAc)
+    public ClockDisplay(int horaAc, int minutosAc, String meridianAc)
     {
+        if (horaAc > 12){
+            veintH = horaAc;
+            horaAc = horaAc - 12;
+            meridianAc = "pm";
+        }
         horas = new NumberDisplay(24);
         horas.setValue(horaAc);
         minutos = new NumberDisplay(60);
         minutos.setValue(minutosAc);
-        horaSt = horas.getDisplayValue() + ":" + minutos.getDisplayValue();
+        meridian = meridianAc;
+        horaSt = horas.getDisplayValue() + ":" + minutos.getDisplayValue() + " " + meridian;
     }
     
         /**
      * Metodo para fijar la hora
      */ 
-    public void setTime(int horaAc, int minutosAc)
+    public void setTime(int horaAc2, int minutosAc2, String meridianAc2)
     {
-        horas.setValue(horaAc);
-        minutos.setValue(minutosAc);
-        horaSt = horas.getDisplayValue() + ":" + minutos.getDisplayValue();
+        if (horaAc2 > 12){
+            veintH = horaAc2;
+            horaAc2 = horaAc2 - 12;
+            meridianAc2 = "pm";
+        }
+        horas.setValue(horaAc2);
+        minutos.setValue(minutosAc2);
+        meridian = meridianAc2;
+        horaSt = horas.getDisplayValue() + ":" + minutos.getDisplayValue() + " " + meridian;
     }
     
         /**
@@ -62,14 +78,35 @@ public class ClockDisplay
      */ 
     public void timeTick()
     {
-        if (minutos.getValue() == 59){
+        if (minutos.getValue() == 59 && veintH < 11){
             minutos.increment();
             horas.increment();
-            horaSt = horas.getDisplayValue() + ":" + minutos.getDisplayValue();
+            meridian = "am";
+            horaSt = horas.getDisplayValue() + ":" + minutos.getDisplayValue() + " " + meridian;
+        }
+        else if (minutos.getValue() == 59 && veintH == 11){
+            minutos.increment();
+            horas.increment();
+            meridian = "m";
+            horaSt = horas.getDisplayValue() + ":" + minutos.getDisplayValue() + " " + meridian;
+        }
+        else if (minutos.getValue() == 59 && veintH >= 12 && veintH < 23){
+            minutos.increment();
+            horas.increment();
+            int horasAc3 = horas.getValue();
+            meridian = "pm";
+            horaSt = horasAc3 + ":" + minutos.getDisplayValue() + " " + meridian; 
+        }
+        else if (minutos.getValue() == 59 && veintH == 23){
+            minutos.increment();
+            horas.increment();
+            meridian = "am";
+            veintH = 0;
+            horaSt = horas.getDisplayValue() + ":" + minutos.getDisplayValue() + " " + meridian;
         }
         else{
             minutos.increment();
-            horaSt = horas.getDisplayValue() + ":" + minutos.getDisplayValue(); 
+            horaSt = horas.getDisplayValue() + ":" + minutos.getDisplayValue() + " " + meridian;
         }
     }
 }
